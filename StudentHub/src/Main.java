cd267cd014b39d761e7cea95ae30d2e2d81a81ca
spillam <spillanem@wit.edu>
2025-05-22 14:24:45 -0400

    Assignment 3 first 2nd task done.

--- StudentHub/src/Main.java
+++ StudentHub/src/Main.java
@@ -5,18 +5,26 @@ import java.sql.Statement;
 
 public class Main {
 
-    public static void connect() {
+    public static void SQLMode() {
         // connection string
         var url = "jdbc:sqlite:Data/assignment3.db";
 
-        try (var conn = DriverManager.getConnection(url);
-             Statement statement = conn.createStatement()) {
+        try (var conn = DriverManager.getConnection(url)) {
+            Statement statement = conn.createStatement();
             System.out.println("Connection to SQLite has been established.");
-            ResultSet rs = statement.executeQuery("SELECT * FROM STUDENT WHERE (MAJOR = 'BSAS')");
+            ResultSet rs = statement.executeQuery("SELECT * FROM STUDENT");
 
+/*
+            statement.executeUpdate("INSERT INTO STUDENT VALUES (10011, 'Ethan', 'Rajkumar', 2026, 'BSCO', 'rajkumare@wit.edu');"); //Add student Ethan
+            statement.executeUpdate("INSERT INTO STUDENT VALUES (10012, 'Chris', 'Le', 2026, 'BSCO', 'lec5@wit.edu');"); //Add student Chris
+*/
+
+//            statement.executeUpdate("DELETE FROM INSTRUCTOR WHERE ID = 20006"); //Daniel was deleted
+
+//            statement.executeUpdate("UPDATE ADMIN SET TITLE = 'Vice-President' WHERE ID = 30002"); //Update Vera's title to be Vice-President
             while (rs.next())
             {
-                System.out.println(rs.getString("NAME") + " is a student in BSAS.");
+                System.out.println(rs.getString("NAME"));
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -24,6 +32,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        connect();
+        SQLMode();
     }
 }
