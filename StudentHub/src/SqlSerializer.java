0b64db5f83c8feda6822d1a1ded221facb5b1c07
EthanRajkumar <rajkumare@wit.edu>
2025-06-17 15:49:41 -0400

    Introduce SQL Serialization

---
+++ StudentHub/src/SqlSerializer.java
@@ -0,0 +1,81 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Locale;
+
+public class SqlSerializer {
+    public static String StudentToSql(Student student, String tableName) {
+        String sp = ", ";
+        return "INSERT INTO STUDENTS VALUES (" +
+                student.GetID() + sp +
+                student.GetFirstName() + sp +
+                student.GetLastName() + sp +
+                student.GetGraduationYear() + sp +
+                student.GetMajor() + sp +
+                student.GetEmail() + ");";
+    }
+
+    public static Student StudentFromSql(ResultSet rs)
+    {
+        try {
+            String name = rs.getString("NAME"),
+                    surname = rs.getString("SURNAME"),
+                    id = rs.getString("ID"),
+                    major = rs.getString("MAJOR"),
+                    email = rs.getString("EMAIL");
+
+            int gradYear = 0;//rs.getInt("GRADYEAR");
+
+            return new Student(name, surname, id, gradYear, major, email);
+        } catch (SQLException e) {
+            System.out.println("Error parsing Student object.");
+            return null;
+        }
+    }
+
+
+    public static String InstructorToSql(Instructor instructor, String tableName) {
+        String sp = ", ";
+        return "INSERT INTO INSTRUCTORS VALUES (" +
+                instructor.GetID() + sp +
+                instructor.GetFirstName() + sp +
+                instructor.GetLastName() + sp +
+                instructor.GetTitle() + sp +
+                instructor.GetYearOfHire() + sp +
+                instructor.GetDepartment() + sp +
+                instructor.GetEmail() + ");";
+    }
+
+    public static String AdminToSql(Admin admin, String tableName) {
+        String sp = ", ";
+        return "INSERT INTO ADMINS VALUES (" +
+                admin.GetID() + sp +
+                admin.GetFirstName() + sp +
+                admin.GetLastName() + sp +
+                admin.GetTitle() + sp +
+                admin.GetOffice() + sp +
+                admin.GetEmail() + ");";
+    }
+
+    public static String CourseToSql(Course course, String tableName) {
+        String sp = ", ";
+        StringBuilder days_string = new StringBuilder(), semesters_string = new StringBuilder();
+        String[] days = course.GetDays(), semesters = course.GetSemesters();
+
+        for (String day : days)
+            days_string.append(day);
+
+        for (String semester : semesters)
+            semesters_string.append(semester);
+
+        return "INSERT INTO COURSES VALUES (" +
+                course.GetCRN() + sp +
+                course.GetTitle() + sp +
+                course.GetDepartment() + sp +
+                course.GetTime() + sp +
+                days_string + sp +
+                semesters_string + sp +
+                course.GetYear() + sp +
+                course.GetCredits() + sp +
+                course.GetSeats() + ");";
+    }
+}
