1489f820f9768924c2e2c2d1b49f23d094d531a3
Christopher Le <lec5@wit.edu>
2025-07-09 14:17:06 -0400

    CreateCourse test case not actually writing to in-memory database, currently troubleshooting with assertions

--- StudentHub/src/AdminTest.java
+++ StudentHub/src/AdminTest.java
@@ -12,11 +12,13 @@ class AdminTest {
 
     private InputStream originalSystemIn;
     private static final String url = "jdbc:sqlite::memory:";
+    private static Connection conn;
 
     @BeforeEach
     void setup() throws Exception {
         originalSystemIn = System.in;   //stores original System.in
-        try (Connection conn = DriverManager.getConnection(url); Statement statement = conn.createStatement()) {
+        conn = DriverManager.getConnection(url);
+        try (Statement statement = conn.createStatement()) {
             statement.execute("CREATE TABLE IF NOT EXISTS COURSE ("
                      + "CRN INTEGER PRIMARY KEY,"
                      + " TITLE txt NOT NULL,"
@@ -28,13 +30,18 @@ class AdminTest {
                      + " CREDITS INTEGER NOT NULL,"
                      + " SEATS INTEGER NOT NULL"
                      + ");");
-            statement.execute("DELETE FROM COURSE");
+            statement.executeUpdate("DELETE FROM COURSE");
         }
+
+
     }
 
     @AfterEach
     void takeDown() throws Exception {
         System.setIn(originalSystemIn); //restores original System.in
+        if (conn != null) {
+            conn.close();
+        }
     }
 
     @Test
@@ -46,24 +53,34 @@ class AdminTest {
         System.setIn(testInputStream);  //set System.in as byte array
 
         Admin tester = new Admin("", "", "", "", "", "");
+
+        //Connection conn = DriverManager.getConnection(url);
+        Statement statement = conn.createStatement();
         tester.CreateCourse();
 
-        Connection conn = DriverManager.getConnection(url);
-        try (Statement statement = conn.createStatement(); ResultSet rs = statement.executeQuery("SELECT CRN, TITLE, DEPARTMENT, TIME, DAYS, SEMESTERS, YEAR, CREDITS, " +
-                "SEATS FROM COURSE WHERE CRN = 77777")) {
-            assertTrue(rs.next());
-            assertEquals(77777, rs.getInt("CRN"));
-            assertEquals("Test Cases 101", rs.getString("TITLE"));
-            assertEquals("BSCO", rs.getString("DEPARTMENT"));
-            assertEquals(12301315, rs.getInt("TIME"));
-            assertEquals("Monday Wednesday", rs.getString("DAYS"));
-            assertEquals("Summer", rs.getString("SEMESTERS"));
-            assertEquals(2025, rs.getInt("YEAR"));
-            assertEquals(3, rs.getInt("CREDITS"));
-            assertEquals(20, rs.getInt("SEATS"));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        //statement.executeUpdate("INSERT INTO COURSE VALUES (77777, 'tEST CASE 101', 'BSCO', 17381738, 'Monday Wednesday', 'Summer', 2025, 3, 20);");
+
+        ResultSet rs = statement.executeQuery("SELECT CRN, TITLE, DEPARTMENT, TIME, DAYS, SEMESTERS, YEAR, CREDITS, SEATS FROM COURSE WHERE CRN = 77777");
+        //assertTrue(rs.next());
+        System.out.println(rs.getInt("CRN"));
+        System.out.println(rs.getString("TITLE"));
+        System.out.println(rs.getString("DEPARTMENT"));
+        System.out.println(rs.getInt("TIME"));
+        System.out.println(rs.getString("DAYS"));
+        System.out.println(rs.getString("SEMESTERS"));
+        System.out.println(rs.getInt("YEAR"));
+        System.out.println(rs.getInt("CREDITS"));
+        System.out.println(rs.getInt("SEATS"));
+
+        assertEquals(77777, rs.getInt("CRN"));
+        assertEquals("Test Cases 101", rs.getString("TITLE"));
+        assertEquals("BSCO", rs.getString("DEPARTMENT"));
+        assertEquals(12301315, rs.getInt("TIME"));
+        assertEquals("Monday Wednesday", rs.getString("DAYS"));
+        assertEquals("Summer", rs.getString("SEMESTERS"));
+        assertEquals(2025, rs.getInt("YEAR"));
+        assertEquals(3, rs.getInt("CREDITS"));
+        assertEquals(20, rs.getInt("SEATS"));
 
     }
 
